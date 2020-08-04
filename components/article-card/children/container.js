d7709d46c9b39ed62f33fefe3d22272f96c44bc5
Hugo Striedinger <striedinger@Hugos-MacBook-Pro.local>
2020-08-03 20:57:38 -0400

    adding spacing classes

--- components/article-card/children/container.js
+++ components/article-card/children/container.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ContainerComponent from '../../container';
 
 const Container = props => {
-  const { children, className, direction, size } = props;
+  const { children, className, direction, margin, padding, width } = props;
   const options = {
     className,
     direction,
-    size,
+    margin,
+    padding,
+    width,
   };
   return (
     <ContainerComponent options={options}>
--- components/container/index.js
+++ components/container/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classnames from 'classnames';
 import css from './styles.module.css';
+import generateSpacingClasses from '../../lib/utils/css/generateSpacingClasses';
 
 const Container = props => {
-  const { children, options: { className, direction = 'column', size = 100 } } = props;
-  const classes = classnames(className, css.container, css[direction], css[`w-${size}`]);
+  const { children, options: { className, direction = 'column', margin = [], padding = [], width = 100 } } = props;
+  const classes = classnames(className, generateSpacingClasses({ padding, margin }), css.container, css[direction], css[`w-${width}`]);
   return (
     <div className={classes}>
       {children}
